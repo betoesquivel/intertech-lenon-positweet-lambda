587fc0a230f7d31ed25942b953ced97ec59d8b5c
Beto Esquivel <ppbeto94@gmail.com>
2016-11-06 04:50:50 +0000

    Adds numpy

--- handler.py
+++ handler.py
@@ -3,13 +3,15 @@ import tweepy
 from vaderSentiment.vaderSentiment import sentiment
 from firebase import firebase
 import random
+import numpy as np
 
 config = json.load(open('.config.json'))
 firebase = firebase.FirebaseApplication(config['databaseURL'], None)
 
 def random_positweeters(size=3):
     positweeters = firebase.get('/positweeters', None)
-    lucky_ones   = random.sample(positweeters, size if len(positweeters) >= size else len(positweeters))
+    lucky_ones   = random.sample(positweeters,
+                                 size if len(positweeters) >= size else len(positweeters))
     return {k: positweeters[k] for k in lucky_ones}
 
 
