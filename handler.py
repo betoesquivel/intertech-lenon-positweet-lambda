1d1b1cb7c5b2d4c0ba34989650042ceb78077dc1
Beto Esquivel <ppbeto94@gmail.com>
2016-11-06 04:34:33 +0000

    Ignores the notebookes

--- handler.py
+++ handler.py
@@ -2,16 +2,19 @@ import json
 import tweepy
 from vaderSentiment.vaderSentiment import sentiment
 from firebase import firebase
-import json
 import random
 
 config = json.load(open('.config.json'))
 firebase = firebase.FirebaseApplication(config['databaseURL'], None)
 
-def tweet(event, context):
+def random_positweeters(size=3):
     positweeters = firebase.get('/positweeters', None)
-    lucky_ones   = random.sample(positweeters, 3 if len(positweeters) >=3 else len(positweeters))
+    lucky_ones   = random.sample(positweeters, size if len(positweeters) >= size else len(positweeters))
+    return {k: positweeters[k] for k in lucky_ones}
+
+
+def tweet(event, context):
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": { k: positweeters[k] for k in lucky_ones }
+        "event": random_positweeters()
     }
