58a63a22de4bd28fe10785e2f14a7e72a165d729
Beto Esquivel <ppbeto94@gmail.com>
2016-11-06 04:16:56 +0000

    Adds all the libs

--- handler.py
+++ handler.py
@@ -1,9 +1,17 @@
 import json
 import tweepy
 from vaderSentiment.vaderSentiment import sentiment
+from firebase import firebase
+import json
+import random
+
+config = json.load(open('.config.json'))
+firebase = firebase.FirebaseApplication(config['databaseURL'], None)
 
 def tweet(event, context):
+    positweeters = firebase.get('/positweeters', None)
+    lucky_ones   = random.sample(positweeters, 3 if len(positweeters) >=3 else len(positweeters))
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        "event": { k: positweeters[lucky_ones] for k in lucky_ones }
     }
